6356698fecb4f59ea56c4a1abc3a93c6d8ef1856
Erbarbar <filipelopez3@gmail.com>
2015-12-18 14:19:59 +0000

    PokéPong

---
+++ Assets/ball_script.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ball_script : MonoBehaviour {
+
+	public GameObject ball;
+	public Rigidbody2D rb2d;
+	private int score1, score2;
+	private float vMax;
+	public Text sc1, sc2;
+	public float vtop;
+	// Use this for initialization
+	void Start () {
+		rb2d.AddForce (Vector2.right * 10, ForceMode2D.Impulse);
+		rb2d.AddTorque (10f);
+		score1 = 0;
+		score2 = 0;
+		vMax = 18;
+		vtop = 0;
+	}
+
+	void FixedUpdate(){
+		if (Mathf.Abs (rb2d.velocity.x) > vtop)
+			vtop = Mathf.Abs (rb2d.velocity.x);
+		if (Mathf.Abs (rb2d.velocity.y) > vtop)
+			vtop = Mathf.Abs (rb2d.velocity.y);
+		if (Mathf.Abs( rb2d.velocity.x) >= vMax)
+			rb2d.velocity = new Vector2 (vMax, rb2d.velocity.y);
+		if (Mathf.Abs( rb2d.velocity.y) >= vMax)
+			rb2d.velocity = new Vector2 (rb2d.velocity.x, vMax);
+	}
+
+	void Update(){
+		sc1.text = "P1: " + score1;
+		sc2.text = "P2: " + score2;
+		if (Input.GetKeyDown("space")){
+			score1 = 0;
+			score2 = 0;
+			ball.transform.position = new Vector2(0f,0f);
+			rb2d.velocity = new Vector2(0f,0f);
+			rb2d.angularVelocity = 0f;
+			rb2d.AddForce (Vector2.right * 10, ForceMode2D.Impulse);
+			rb2d.AddTorque (10f);
+
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.tag == "right") {
+			score1 ++;
+		}
+		if (other.gameObject.tag == "left") {
+			score2 ++;
+		}
+	}
+}
---
+++ Assets/pad_script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pad_script : MonoBehaviour {
+
+	public GameObject pad;
+	private float posDif;
+	private float posAct;
+	private float posMax, posMin;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		posAct =  0f;
+		posMax =  5f;
+		posMin = -5f;
+		speed = 0.25f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pad.gameObject.name == "pad1")
+			posDif = Input.GetAxisRaw ("Custom1");
+		if (pad.gameObject.name == "pad2")
+			posDif = Input.GetAxisRaw ("Custom2");
+		posAct += posDif * speed;
+
+		if (posAct >= posMax)
+			posAct = posMax;
+		if (posAct <= posMin)
+			posAct = posMin;
+
+		pad.transform.position = new Vector2(pad.transform.position.x, posAct);
+	}
+}
